7feda40c3e53a5ebae8113b98614badfdb8aa8b6
Anju <sharmanju80@gmail.com>
2018-09-10 23:25:40 +0530

    update controller annotation.

--- src/main/java/com/synectiks/search/controllers/SearchController.java
+++ src/main/java/com/synectiks/search/controllers/SearchController.java
@@ -13,11 +13,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.synectiks.commons.constants.IConsts;
 import com.synectiks.commons.entities.search.ESEvent;
@@ -31,7 +31,7 @@ import com.synectiks.search.utils.IESUtils;
 /**
  * @author Rajesh
  */
-@Controller
+@RestController
 @RequestMapping(path = IApiController.API_PATH
 		+ IApiController.URL_SEARCH, method = RequestMethod.POST)
 @CrossOrigin
